207eb42b5e23a29f1d856010fe081ef48f68c8e1
Trond Nordheim <trond@nordheim.io>
2017-03-29 16:34:24 +0200

    Configure YouCompleteMe on install

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+set -e
+
 BASEDIR=$(pwd -P)
 
 ln -sf $BASEDIR ~/.vim
 ln -sf $BASEDIR/vimrc ~/.vimrc
+
+(cd bundle/YouCompleteMe && ./install.py --clang-completer --omnisharp-completer --gocode-completer --tern-completer)
